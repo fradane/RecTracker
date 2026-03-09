a6c0eccc254a1d5136ac823b105ea3f2be4e6715
fradane <francesco.danese@mail.polimi.it>
2026-03-09 18:53:31 +0100

    Initial Commit

---
+++ RecTracker/RecTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecTrackerApp.swift
+//  RecTracker
+//
+//  Created by Francesco Danese on 09/03/26.
+//
+
+import SwiftUI
+
+@main
+struct RecTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
